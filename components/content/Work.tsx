ec621fadf1f8a2b1e48685c725be695d83ba7d7d
Kimmie Arvidsson <kiarvidsson@gmail.com>
2022-09-14 22:25:40 +0200

    Added some style and social media icons. Added sass for future use.

--- components/content/Work.tsx
+++ components/content/Work.tsx
@@ -5,11 +5,11 @@ export default function Work({ header, img, text, url, imgRightSide }) {
   const sideOfParagraphToRenderPicture =
     imgRightSide == true ? classes.workright : "";
   return (
-    <div className={classes.work + " " + sideOfParagraphToRenderPicture}>
-      <h1 className={classes.workheader}>{header}</h1>
-      <img src={img} alt="" />
-      <p className={classes.worktext}>{text}</p>
-      <div className={classes.learnmore}>
+    <div className={classes.work}>
+      <h1 className={classes.work__header}>{header}</h1>
+      <img className={sideOfParagraphToRenderPicture} src={img} alt="" />
+      <p className={classes.work__text}>{text}</p>
+      <div className={classes.work__learnmore}>
         <Link href={url}> Learn more here!</Link>
       </div>
     </div>
