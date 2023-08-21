bd86e3367a429393f3936d7d314f055f67c88671
Kimmie Arvidsson <kiarvidsson@gmail.com>
2023-08-21 20:56:49 +0200

    Added some desktop design

--- components/work/Work.tsx
+++ components/work/Work.tsx
@@ -14,30 +14,32 @@ interface param {
 export default function Work(props: param) {
   return (
     <div className={classes.work}>
-      <h1 className={classes.work__header}>{props.header}</h1>
-      <div className={classes.work__imgcontainer}>
-        <Link href={props.url ? props.url : '/'}>
-          <Image
-            alt=""
-            src={props.img}
-            sizes="100vw"
-            style={{ height: 'auto', width: '100%', objectFit: 'cover' }}
-          />
-        </Link>
+      <div style={{ maxWidth: '45rem' }}>
+        <h1 className={classes.work__header}>{props.header}</h1>
+        <div className={classes.work__imgcontainer}>
+          <Link href={props.url ? props.url : '/'}>
+            <Image
+              alt=""
+              src={props.img}
+              sizes="100vw"
+              style={{ height: 'auto', width: '100%', objectFit: 'cover' }}
+            />
+          </Link>
+        </div>
+        <Tag text={props.tags} />
+        <div className={classes.work__textbox}>
+          {props.text.split('\n').map((line, index) => (
+            <p className={classes.work__text} key={index}>
+              {line}
+            </p>
+          ))}
+        </div>
+        {props.url && (
+          <Link href={props.url}>
+            <button className={classes.btn}>Visit</button>
+          </Link>
+        )}
       </div>
-      <Tag text={props.tags} />
-      <div className={classes.work__textbox}>
-        {props.text.split('\n').map((line, index) => (
-          <p className={classes.work__text} key={index}>
-            {line}
-          </p>
-        ))}
-      </div>
-      {props.url && (
-        <Link href={props.url}>
-          <button className={classes.btn}>Visit</button>
-        </Link>
-      )}
     </div>
   )
 }
